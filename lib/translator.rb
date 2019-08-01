191052150e2148ae656d4d705c437a54304e2571
Emily G <aspentree79@gmail.com>
2019-08-01 12:46:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_path)
   file = File.read("./lib/emoticons.yml")
   emoticons = YAML.load_file("./lib/emoticons.yml")
 
-  library = { "get_meaning" => { }
+  library = { "get_meaning" => { },
     # emoticons.each do |key, value|
     "get_emoticon" => { } }
 
